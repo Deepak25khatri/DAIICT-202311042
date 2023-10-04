33e9df780ec88ff7c41efcfc173eb9e1c3e576ea
Deepak25khatri <87187272+Deepak25khatri@users.noreply.github.com>
2023-10-04 11:29:18 +0530

    lab_assign4

---
+++ fibo.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+def fib(n):
+    a,b = 0,1
+    while a<n:
+        print(a,end="")
+        a,b = b,a+b
+    print()
+    
+def fib2(n): # return Fibonacci series up to n
+    result = []
+    a, b = 0, 1
+    while a < n:
+        result.append(a)
+        a, b = b, a+b
+    return result
