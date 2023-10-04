02f4b4d5980d6b835f2ba314130fc0e0dec21c64
Deepak25khatri <87187272+Deepak25khatri@users.noreply.github.com>
2023-10-04 13:29:04 +0530

    lab_assign4

--- matrix.py
+++ matrix.py
@@ -1,19 +1,40 @@
 import numpy as np
-def matrix_add(matrrixx,matrixx2):
+def matrix_add(matrix1,matrix2):
     try:
-        result  = np.add(matrrixx1,matrrixx2)
+        result  = np.add(matrix1,matrix2)
         return result
     except ValueError:
         return "Matrix dimension must be same"
-def matrix_subtract(matrrixx1,matrixx2):
+def matrix_subtract(matrix1,matrix2):
     try:
-        result  = np.subtract(matrrixx1,matrrixx2)
+        result  = np.subtract(matrix1,matrix2)
         return result
     except ValueError:
         return "Matrix dimension must be same"
-def matrix_mul(matrrixx1,matrixx2):
+def matrix_mul(matrix1,matrix2):
     try:
-        result  = np.dot(matrrixx1,matrrixx2)
+        result  = np.dot(matrix1,matrix2)
         return result
     except ValueError:
-        return "Matrix dimension must be same"
+        return "Matrix are not suitable for multiplication "
+if __name__ == "__main__":
+    # matrix_a = np.random.randint(1, 10, size=(3, 3))
+    # matrix_b = np.random.randint(1, 10, size=(3, 3))
+
+    print("Matrix A:")
+    print(matrix_a)
+
+    print("Matrix B:")
+    print(matrix_b)
+
+    addition_result = matrix_add(matrix_a, matrix_b)
+    print("Matrix Addition Result:")
+    print(addition_result)
+
+    subtraction_result = matrix_subtract(matrix_a, matrix_b)
+    print("Matrix Subtraction Result:")
+    print(subtraction_result)
+
+    multiplication_result = matrix_mul(matrix_a, matrix_b)
+    print("Matrix Multiplication Result:")
+    print(multiplication_result)
